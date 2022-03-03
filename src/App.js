d1228f52cd7bbc52114cf8cf6d4b51d33d7df565
SaifSushi <alushisaif@gmail.com>
2022-03-03 03:48:32 -0600

    testing requests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [jwt, setJwt] = useState(null)
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${jwt}`
+  }
+
+  const authURL = 'http://localhost:8000/api/token/' 
+
+  useEffect(() => {
+    axios.post(authURL, {username: 'alushi', password: '1234'})
+    .then((res) => {
+      console.log(res)
+      setJwt(res.data.access)
+      console.log("Response: " + res.data.access)
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+  const getURL = 'http://localhost:8000/rotmg/collection/'
+    const getFunc =  async () => {
+      try{
+        await axios.get(getURL, {headers: headers})
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+      } catch(err){
+        console.log(err)
+      }
+    }
+
+    const testPost = async () => {
+        try{
+          await axios.post()
+        } catch(err){
+          console.log(err)
+        }
+    }
+
+   useEffect(() => {console.log(jwt)}, [jwt])
+    return (
+      <>
+    <p>hello</p>
+    <button onClick={() => getFunc() }>
+      Get Moments
+    </button>
+    </>
   );
 }
 
